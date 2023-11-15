9e8bb8a2ced7ff18c1b49fa7265c1e355e7a26c7
Tosin Balogun <tosinbalogun1@gmail.com>
2023-11-15 19:40:34 +0100

    renamed variables

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int lineCount = 0;
-        int wordCount = 0;
+        int rowCount = 0;
+        int characterCount = 0;
         while (true) {
             System.out.print("> ");
             String line = scanner.nextLine();
@@ -12,11 +12,11 @@ public class Main {
                 break;
             }
             System.out.println("Echo:" + line);
-            lineCount +=1;
-            wordCount +=line.length();
+            rowCount +=1;
+            characterCount +=line.length();
         }
         System.out.println("Hello world!");
-        System.out.println("Line count: " + lineCount);
-        System.out.println("Word count: " + wordCount);
+        System.out.println("Row count: " + rowCount);
+        System.out.println("Character count: " + characterCount);
     }
 }
