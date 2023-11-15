60be8f19c5b67e841715349223cab1912bc7084a
Tosin Balogun <tosinbalogun1@gmail.com>
2023-11-15 19:47:25 +0100

    delete logic

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int rowCount = 0;
-        int characterCount = 0;
         while (true) {
             System.out.print("> ");
             String line = scanner.nextLine();
@@ -12,11 +10,6 @@ public class Main {
                 break;
             }
             System.out.println("Echo:" + line);
-            rowCount +=1;
-            characterCount +=line.length();
         }
-        System.out.println("Hello world!");
-        System.out.println("Row count: " + rowCount);
-        System.out.println("Character count: " + characterCount);
     }
 }
